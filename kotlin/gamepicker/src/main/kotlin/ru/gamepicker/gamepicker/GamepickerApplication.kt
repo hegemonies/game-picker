3abdaf0459dea4003fea2f447f81103b3b663f7e
daniil dyachenko <hegemonies@yandex.ru>
2021-10-03 01:46:01 +0700

    [backend] Инициализирует проект

---
+++ kotlin/gamepicker/src/main/kotlin/ru/gamepicker/gamepicker/GamepickerApplication.kt
@@ -0,0 +1,11 @@
+package ru.gamepicker.gamepicker
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GamepickerApplication
+
+fun main(args: Array<String>) {
+	runApplication<GamepickerApplication>(*args)
+}
---
+++ kotlin/gamepicker/src/test/kotlin/ru/gamepicker/gamepicker/GamepickerApplicationTests.kt
@@ -0,0 +1,13 @@
+package ru.gamepicker.gamepicker
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GamepickerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
